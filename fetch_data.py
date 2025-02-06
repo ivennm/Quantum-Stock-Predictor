4c33d9a52ac5349f0599cb2d3a49767c1b84a86a
Ivan Mendez <166038157+ivennm@users.noreply.github.com>
2025-02-06 14:49:14 -0500

    Update fetch_data.py

--- fetch_data.py
+++ fetch_data.py
@@ -1,3 +1,11 @@
 import yfinance as yf
 import pandas as pd
 
+
+
+def get_data(stock,period="1y", interval="1d"):
+    ...
+
+if __name__ == "__main__":
+    data = get_stock_data("AAPL")
+    print(data.head())
